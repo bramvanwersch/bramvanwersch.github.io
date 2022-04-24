961c1b207fc9130f99705caaa2e300052815bc5c
bram van wersch <eckelradekip10@gmail.com>
2022-04-24 09:33:41 +0200

    machine mining now is done :)

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,5 +1,5 @@
 
-function nextImage(img_object, change, total_images){
+function nextImage(img_object, change, total_images, base_image_name){
     var newValue = (parseInt(img_object.dataset.nr) + change)
     if (newValue < 0){
         newValue = total_images -1
@@ -8,6 +8,5 @@ function nextImage(img_object, change, total_images){
         newValue = 0
     }
     img_object.dataset.nr = newValue
-    img_object.src = "data/roguelike" + newValue + ".png"
-    //var value = document.getElementById(id).value
+    img_object.src = "data/" + base_image_name + newValue + ".png"
 }
